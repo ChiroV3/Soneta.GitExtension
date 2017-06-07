ca06c2c46606995eeebfa72cd0d5d6154d0b1b64
ChiroV3 <xnarx@o2.pl>
2017-06-07 12:26:52 +0200

    Dodana pozycja dodatku w enova365

---
+++ Soneta.GitExtension/Extender/Menu.cs
@@ -0,0 +1,11 @@
+﻿using Soneta.Business.UI;
+using Soneta.GitExtension.Extender;
+
+[assembly: FolderView("Soneta.GitExtension/Statystyki GIT",
+    Priority = 10,
+    Description = "Statystyki użytkowników repo",
+    ObjectType = typeof(GitUserActivity),
+    ObjectPage = "GitUserActivity.pageform.xml",
+    ReadOnlySession = false,
+    ConfigSession = false
+)]
